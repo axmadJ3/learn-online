42865b7afc793ab00c6218a2035ba4c3b5578efb
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-05-27 01:32:30 +0500

    Enhance user creation API with async support and validation improvements; update dependencies for email validation

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import re
 import uuid
 
 from fastapi import FastAPI, HTTPException
+from fastapi.routing import APIRouter
 from sqlalchemy import Column, Boolean, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.dialects.postgresql import UUID
 from pydantic import BaseModel, EmailStr, field_validator
+import uvicorn
 
 import settings
 
@@ -67,7 +69,7 @@ class UserDAL:
 ############################################################
 
 
-LETTER_MATCH_PATTERN = re.compile(r"^а-аЯ-Яа-zA-Z\-]+$")
+LETTER_MATCH_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z\-]+$")
 
 
 class TunedModel(BaseModel):
@@ -90,7 +92,7 @@ class UserCreate(BaseModel):
     surname: str
     email: EmailStr
     
-    @field_validator('name', mode="before")
+    @field_validator('name', mode="after")
     @classmethod
     def validate_name(cls, value):
         if not LETTER_MATCH_PATTERN.match(value):
@@ -110,7 +112,45 @@ class UserCreate(BaseModel):
     
 
 ############################################################
-# BLOCK WITH API MODELS
+# BLOCK WITH API ROUTES
 ############################################################
 
 
+app = FastAPI(title="online-learning")
+
+user_router = APIRouter()
+
+
+async def _create_new_user(body: UserCreate) -> ShowUser:
+    async with async_session() as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(
+                name=body.name,
+                surname=body.surname,
+                email=body.email,
+            )
+            return ShowUser(
+                user=user.user_id,
+                name=user.name,
+                surname=user.surname,
+                email=user.email,
+                is_active=user.is_active,
+            )
+            
+@user_router.post('/', response_model=ShowUser)
+async def create_user(body: UserCreate) -> ShowUser:
+    return await _create_new_user(body)
+
+
+# create the instance for the routes
+main_api_router = APIRouter()
+
+# set routes to the app instance
+main_api_router.include_router(user_router, prefix='/user', tags=['user'])
+app.include_router(main_api_router)
+
+if __name__ == "__main__":
+    # run app on the host and port
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
