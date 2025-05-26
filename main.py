e8a8e24e07cb0c015548993120e3919b63b40f81
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-05-27 00:15:57 +0500

    Add validation for user name and surname in UserCreate model and enhance TunedModel configuration

--- main.py
+++ main.py
@@ -67,3 +67,48 @@ class UserDAL:
 ############################################################
 
 
+LETTER_MATCH_PATTERN = re.compile(r"^а-аЯ-Яа-zA-Z\-]+$")
+
+
+class TunedModel(BaseModel):
+    class Config:
+        """tels pydantic to convert any non dict obj to json"""
+        
+        orm_mode = True
+
+
+class ShowUser(TunedModel):
+    user_id: uuid.UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+        
+        
+class UserCreate(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+    
+    @field_validator('name')
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Name should contains only letters"
+            )
+        return value
+        
+    @field_validator('surname')
+    def validate_surname(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Surname should contains only letters"
+            )
+        return value
+    
+
+############################################################
+# BLOCK WITH API MODELS
+############################################################
+
+
