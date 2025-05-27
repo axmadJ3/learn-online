37201abfe98a05f78eab8c657c2ce2a8f26dc77c
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-05-27 05:39:15 +0500

    Add Alembic configuration and migration scripts; update psycopg2 dependency

--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ env = Env()
 
 REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
-    default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres"
+    default="postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
 )
